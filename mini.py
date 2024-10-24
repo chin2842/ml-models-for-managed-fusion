5cde9624cacd6527d4a05c20aa4a764b710dd3ec
chin2842 <chinmaybabar46@gmail.com>
2024-10-24 10:43:34 +0530

    Removed app.py and made enhancements to rest

--- mini.py
+++ mini.py
@@ -6,22 +6,16 @@ from typing import Iterable
 import numpy as np
 import torch
 
-# Set up logging
-logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger()
 
-class mini:
+class App():  # Class renamed to match the entry point
     def __init__(self):
-        # Set the TRANSFORMERS_CACHE environment variable to a directory within the app
-        os.environ["TRANSFORMERS_CACHE"] = "/app/transformers"
-
-        # Load the tokenizer and model
         self.tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
         self.model = AutoModel.from_pretrained('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
 
     # Mean Pooling
     def mean_pooling(self, model_output, attention_mask):
-        token_embeddings = model_output[0]  # First element of model_output contains all token embeddings
+        token_embeddings = model_output[0]
         input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
         return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
@@ -29,21 +23,16 @@ class mini:
         model_input = dict(zip(names, X))
         text = model_input["text"]
 
-        with torch.inference_mode():  # Allows torch to run more quickly
-            # Tokenize sentences
+        with torch.inference_mode():
             encoded_input = self.tokenizer(text, padding=True, truncation=True, return_tensors='pt')
-            log.debug('Encoded input: %s', str(encoded_input))
             model_output = self.model(**encoded_input)
-            log.debug('Model output: %s', str(model_output))
-
-            # Perform pooling
             sentence_embeddings = self.mean_pooling(model_output, encoded_input['attention_mask'])
-            # Normalize embeddings
             sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=-1)
-            # Fixing the shape of the embeddings to match (1, 384)
             final = [sentence_embeddings.squeeze().cpu().detach().numpy().tolist()]
-
         return final
 
     def class_names(self) -> Iterable[str]:
         return ["vector"]
+
+if __name__ == "__main__":
+    model = App()  # Instantiate the class
