c40cbd6285c1a85c83ef81088fd11af90759ac14
chin2842 <chinmaybabar46@gmail.com>
2024-10-24 14:05:50 +0530

    Made changes to Dockerfile and mini.py

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,33 @@
-# Use a slim Python 3.9 image
+#It is important that python version is 3.x-slim for seldon-core
 FROM python:3.9-slim
-
-# Set a working directory
+# Whatever directory(folder)the python file for your python class, Dockerfile, and
+# requirements.txt is in should be copied then denoted as the work directory.
+COPY . /app
 WORKDIR /app
 
-# Copy the entire application into /app
-COPY . .
-
-# Set up cache directory
-RUN mkdir -p /app/cache && \
-    chown -R 8888 /app/cache
-
-# Install the requirements
+# The requirements file for the Docker container
 COPY requirements.txt requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Change ownership of the /app directory
-RUN chown -R 8888 /app
+# Copy source code
+COPY . .
 
-# Expose the required port
+# GRPC - Allows Managed Fusion to do a Remote Procedure Call
 EXPOSE 5000
 
-# Define environment variables
-ENV HF_HOME=/app/cache \
-    MODEL_NAME=mini \
-    APP_MODULE=mini:mini \
-    SERVICE_TYPE=MODEL \
-    PERSISTENCE=0
+# Define environment variable for seldon-core
+# !!!MODEL_NAME must be the EXACT same as the python file & python class name!!!
+ENV MODEL_NAME mini
+ENV SERVICE_TYPE MODEL
+ENV PERSISTENCE 0
 
-# Command to start the Seldon microservice
-CMD ["seldon-core-microservice", "mini", "--service-type", "MODEL", "--persistence", "0"]
+# Changing active directory folder (same one as above on lines 5 & 6) to default user, required for Managed Fusion
+RUN chown -R 8888 /app
+
+# Command to wrap python class with seldon-core to allow it to be usable in Managed Fusion
+CMD ["sh", "-c", "seldon-core-microservice $MODEL_NAME --service-type $SERVICE_TYPE --persistence $PERSISTENCE"]
+
+# You can use the following if You need shell features like environment variable expansion or
+# You need to use shell constructs like pipes, redirects, etc.
+# See https://docs.docker.com/reference/dockerfile/#cmd for more details.
+# CMD exec seldon-core-microservice $MODEL_NAME --service-type $SERVICE_TYPE --persistence $PERSISTENCE
--- mini.py
+++ mini.py
@@ -1,48 +1,51 @@
 import logging
 import os
+
 from transformers import AutoTokenizer, AutoModel
 from torch.nn import functional as F
 from typing import Iterable
 import numpy as np
 import torch
 
-# Set up logging
-logging.basicConfig(level=logging.INFO)
 log = logging.getLogger()
 
-# Set the cache directory to a writable path
-os.environ["HF_HOME"] = "/app/cache"
-
-class mini:
+class mini():
     def __init__(self):
-        log.info("Loading tokenizer and model...")
-        self.tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
-        self.model = AutoModel.from_pretrained('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+        # Set the TRANSFORMERS_CACHE environment variable to a system-wide location
+        os.environ["HF_HOME"] = "C:\\ProgramData\\transformers"
 
-    # Mean Pooling
+        self.tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+        self.model= AutoModel.from_pretrained('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+
+    #Mean Pooling
     def mean_pooling(self, model_output, attention_mask):
-        token_embeddings = model_output[0]
+        token_embeddings = model_output[0] #First element of model_output contains all token embeddings
         input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
         return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
-    def predict(self, X: np.ndarray, names=None, **kwargs):
-        try:
-            model_input = dict(zip(names, X))
-            text = model_input["text"]
+    def predict(self, X:np.ndarray, names=None, **kwargs):
+        #   In Managed Fusion there are several variables passed in the numpy array with the Milvus Query stage,
+        #   Encode to Milvus index stage, and Vectorize Seldon index and query stage:
+        #   [pipeline, bool, and text]. Text is what variable will be encoded, so that is what will be set to 'text'
+        #   When using the Machine Learning stage, the input map keys should match what what is in this file.
 
-            with torch.inference_mode():
-                encoded_input = self.tokenizer(text, padding=True, truncation=True, return_tensors='pt')
-                model_output = self.model(**encoded_input)
-                sentence_embeddings = self.mean_pooling(model_output, encoded_input['attention_mask'])
-                sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=-1)
-                final = [sentence_embeddings.squeeze().cpu().detach().numpy().tolist()]
-            return final
-        except Exception as e:
-            log.error(f"Prediction error: {str(e)}")
-            return [None]
+        model_input = dict(zip(names, X))
+        text = model_input["text"]
+
+        with torch.inference_mode(): # Allows torch to run more quickly
+            # Tokenize sentences
+            encoded_input = self.tokenizer(text, padding=True, truncation=True, return_tensors='pt')
+            log.debug('encoded input',str(encoded_input))
+            model_output = self.model(**encoded_input)
+            log.debug('model output',str(model_output))
+
+            # Perform pooling. In this case, max pooling.
+            sentence_embeddings = self.mean_pooling(model_output, encoded_input['attention_mask'])
+            # Normalize embeddings, because Managed Fusion likes it that way.
+            sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=-1)
+            # Fixing the shape of the emebbedings to match (1, 384).
+            final = [sentence_embeddings.squeeze().cpu().detach().numpy().tolist()]
+        return final
 
     def class_names(self) -> Iterable[str]:
         return ["vector"]
-
-if __name__ == "__main__":
-    model = mini()  # Instantiate the class
